056f8903f492a7eaa08b2076d87996a7040db510
jkheyyad.ext <jkheyyad.ext@softwaymedical.fr>
2025-05-18 12:20:38 +0200

    app working with older browser

--- gwt-app/src/main/webapp/react-app/webpack.config.js
+++ gwt-app/src/main/webapp/react-app/webpack.config.js
@@ -14,12 +14,8 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
+          loader: 'babel-loader'
         }
       },
       {
