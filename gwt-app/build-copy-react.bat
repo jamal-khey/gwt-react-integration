d6eaf0e743e049b1dcbede60d6a260cf1e4e4997
jkheyyad.ext <jkheyyad.ext@softwaymedical.fr>
2025-05-18 13:08:12 +0200

    add script to build the react app

---
+++ gwt-app/build-copy-react.bat
@@ -0,0 +1,10 @@
+
+@echo off
+REM Build React app
+cd src\main\webapp\react-app
+call npm run build
+
+REM Copy React build output to GWT war directory
+xcopy /E /Y dist ..\..\..\..\..\war\react-app\dist
+
+cd ..\..\..\..
--- gwt-app/build.bat
+++ gwt-app/build.bat
@@ -3,5 +3,8 @@ REM Path to Maven installation
 set MAVEN_HOME=C:\dev\plateform\maven\apache-maven-3.2.5
 
 set JAVA_HOME=C:\dev\plateform\java\jdk1.8.0_72
+
+
+
 REM Use custom settings.xml and repository location
 "%MAVEN_HOME%\bin\mvn" -s "C:\dev\plateform\maven\apache-maven-3.2.5settings.xml" -Dmaven.repo.local="C:\dev\plateform\maven\testrepo" %*
